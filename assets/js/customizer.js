bdcd11d5d7ed2c10afec9231358689adbedc6568
Kien T <kienpc81089@gmail.com>
2016-04-28 17:50:27 +0700

    Change upsell links

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -1,4 +1,4 @@
 
 jQuery( document ).ready( function( $ ){
-    $('#customize-info .accordion-section-title').append('<a target="_blank" style="text-transform: uppercase; background: #D54E21; color: #fff; font-size: 10px; line-height: 14px; padding: 2px 5px; display: inline-block;" href="https://www.famethemes.com/themes/codilight/?utm_source=codilight_lite_customizer&utm_medium=text_link&utm_campaign=codilight_lite">Upgrade to Codilight Premium</a>');
+    $('#customize-info .accordion-section-title').append('<a target="_blank" style="text-transform: uppercase; background: #D54E21; color: #fff; font-size: 10px; line-height: 14px; padding: 2px 5px; display: inline-block;" href="https://www.famethemes.com/themes/codilight-lite?utm_source=codilight_lite_customizer&utm_medium=text_link&utm_campaign=codilight_lite#compare">Upgrade to Codilight Premium</a>');
 } );
